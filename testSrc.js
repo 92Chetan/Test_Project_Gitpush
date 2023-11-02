00983358eb13aaddf840aabd6beb93067a7975b7
92Chetan <chetansinghal@outlook.com>
2023-11-02 23:52:43 +0530

    addingTwoNumber

---
+++ testSrc.js
@@ -0,0 +1,7 @@
+// document.write("Hello World");
+
+
+let Name1 = "Maxwell Wright"; let Name2 = "Raja Villarreal"; let Name3 = "Helen Richards";
+let Phone1 = '(0191) 719 6495'; let Phone2 = '0866 398 2895'; let Phone3 = '0800 1111';
+let Email1 = "Curabitur.egestas.nunc@nonummyac.co.uk"; let Email2 = "posuere.vulputate@sed.com"; let Email3 = "libero@convallis.edu";
+console.log(Name1,"/",Phone1,"/",Email1);
